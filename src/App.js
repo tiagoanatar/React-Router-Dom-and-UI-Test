ca2f0c760f45d7094ac1224e1f268218b81a3336
tiagoanatar <35281635+tiagoanatar@users.noreply.github.com>
2021-06-23 23:08:21 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './js/front.js'
 
 // react
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // components
 import Header from './components/header'
@@ -24,19 +24,14 @@ import Footer from './components/Footer'
 import Map from './screens/Map'
 import Army from './screens/Army'
 
-function Home() {
-  return (
-
-  <>
-    <div className='title'><strong>Basic Table</strong></div>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat, ex sed pretium pellentesque, ex erat varius odio, in cursus enim dui a ipsum. Duis consequat nibh eu mi commodo, a pellentesque quam placerat. Praesent vitae velit fermentum, mollis neque sit amet, tempus nisi. Suspendisse non sollicitudin nisi. Pellentesque ultricies vestibulum ligula vulputate lobortis. Nunc eu diam lorem. Quisque orci orci, fringilla vel dictum ut, vestibulum quis augue. Sed sollicitudin finibus tincidunt. Vestibulum porta facilisis justo a sagittis.</p>
-  </>
-
-  )
-}
-
 function App() {
+
+  const [task, setTaask] = useState([])
+
+  useEffect(() => {
+
+  }, [])
+
   return (
 
     <Router>
@@ -69,7 +64,7 @@ function App() {
                       <Army />
                     </Route>
                     <Route path='/test'>
-                      <Home />
+                      
                     </Route>
                     <Route path='/'>
                       <Map />
